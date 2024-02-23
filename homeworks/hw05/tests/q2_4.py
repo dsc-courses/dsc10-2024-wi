817aa805d744b4bde4918b08363d59ffce5c7041
ylesia-wu <130006120+ylesia-wu@users.noreply.github.com>
2024-02-22 16:29:01 -0800

    added hw05

---
+++ homeworks/hw05/tests/q2_4.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q2_4',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(theoretical_counts_mean, float) and isinstance(actual_counts_mean, float)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> int(theoretical_counts_mean * 10**13 % 10**4) == 9345  # Try again!\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ homeworks/hw05/tests/q3_2.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3_2',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(exact_sd, float)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> int(exact_sd * 10 ** 2) == 2 # Try again!\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
