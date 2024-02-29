7d75aca75179fe309a3bd07be4a6bc4202d591d5
ylesia-wu <130006120+ylesia-wu@users.noreply.github.com>
2024-02-29 15:54:58 -0800

    added hw06

---
+++ homeworks/hw06/tests/q1_5.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_5',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> type(bad_choices) == list and all(choice in [1, 2, 3, 4] for choice in bad_choices)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
